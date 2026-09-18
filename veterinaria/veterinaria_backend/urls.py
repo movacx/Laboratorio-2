57e9c1df6d923aae887d90fa5837a920f05fc0f1
Fabian Beita <fabianbeita36@gmail.com>
2026-09-17 21:18:54 -0600

    Fix

--- veterinaria/veterinaria_backend/urls.py
+++ veterinaria/veterinaria_backend/urls.py
@@ -5,8 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # TokenAuthentication:
-    # POST /api/token/ con username + password.
     path('api/token/', obtain_auth_token, name='api_token'),
 
     path('clinica/', include('clinica.urls')),
